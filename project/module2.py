27f93b6575b9c0363073a170df2df78dd92fe657
Fabian Senf <senf@tropos.de>
2020-01-23 08:40:06 +0100

    added missing second module

---
+++ project/module2.py
@@ -0,0 +1,48 @@
+import sys
+
+def func2( x ):
+
+    '''
+    Test function 1.
+
+
+    Parameters
+    ----------
+    x : float
+        test para
+
+
+    Returns
+    -------
+    y : float
+        test para
+
+    '''
+
+    y = 2 * x
+
+    return y
+
+
+def func3( x ):
+
+    '''
+    Test function 1.
+
+
+    Parameters
+    ----------
+    x : float
+        test para
+
+
+    Returns
+    -------
+    y : float
+        test para
+
+    '''
+
+    y = 2 * x
+
+    return y
